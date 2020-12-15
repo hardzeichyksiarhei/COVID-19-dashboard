17a074507ec3bf395c6cf3575959cd9345a2ac08
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-12-15 13:32:48 +0300

    init: start covid-19

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
