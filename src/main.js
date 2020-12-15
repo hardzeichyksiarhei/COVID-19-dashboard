1a29bffb8480fc7656033c4129df4d47cbcc5e9d
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-12-15 18:41:44 +0300

    fix: connect primevue

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import { createApp } from "vue";
 import store from "./store";
 import router from "./router";
 
+import PrimeVue from "primevue/config";
+
 import App from "./App.vue";
 
-export const app = createApp(App);
+/* Connect Styles */
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(PrimeVue);
 
 app.mount("#app");
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="dashboard">Dashboard</div>
+  <div class="dashboard">
+    <Card>
+      <template #header>
+        <img
+          alt="user header"
+          src="https://images.unsplash.com/photo-1608035910534-d29aea04943e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1317&q=80"
+        />
+      </template>
+      <template #title> Advanced Card </template>
+      <template #content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </template>
+      <template #footer>
+        <Button icon="pi pi-check" label="Save" />
+        <Button
+          icon="pi pi-times"
+          label="Cancel"
+          class="p-button-secondary"
+          style="margin-left: 0.5em"
+        />
+      </template>
+    </Card>
+  </div>
 </template>
 
 <script>
+import Card from "primevue/card";
+
 export default {
   name: "Dashboard",
-  components: {},
+  components: { Card },
 };
 </script>
