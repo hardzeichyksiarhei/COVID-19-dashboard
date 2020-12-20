8d0153e6106037c4ec46c9416a8fb43cddb41391
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-12-20 15:52:38 +0300

    feat: computed basicData

--- src/components/cards/VChart.vue
+++ src/components/cards/VChart.vue
@@ -1,47 +1,45 @@
 <template>
-    <div>
-        <Chart type="bar" :data="basicData" />
-    </div>
+  <div v-if="countries">
+    <Chart type="bar" :data="basicData" />
+  </div>
 </template>
 
 
 <script>
-import Chart from 'primevue/chart';
+import Chart from "primevue/chart";
 import covidService from "../../services/covid.services";
 import { mapGetters } from "vuex";
 // import VIndicatorsTypesSelect from "../cards/VIndicators"
 
 export default {
-    name: "VChart",
-    components: { Chart },
-    data() {
-        return {
-            basicData: {},
-        };
+  name: "VChart",
+  components: { Chart },
+  data() {
+    return {
+      countries: null,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      currentIndicator: "countries/currentIndicator",
+    }),
+    basicData() {
+      return {
+        labels: Object.keys(this.countries[this.currentIndicator.key]),
+        datasets: [
+          {
+            label: this.currentIndicator.label,
+            backgroundColor: "#42A5F5",
+            data: Object.values(this.countries[this.currentIndicator.key]),
+          },
+        ],
+      };
     },
-    computed: {
-        ...mapGetters({
-            currentIndicator: "countries/currentIndicator",
-        }),
-    },
-    mounted(){
-        covidService.getHistoricalAll().then(res => {
-            this.basicData = {
-                    labels: Object.keys(res[`${this.currentIndicator.key}`]),
-                    datasets: [
-                        {
-                            label: `${this.currentIndicator.label}`,
-                            backgroundColor: '#42A5F5',
-                            data: Object.values(res[`${this.currentIndicator.key}`]) 
-                        },
-                    ]
-                }
-            }
-        );
-    }
-}
+  },
+  async created() {
+    this.countries = await covidService.getHistoricalAll();
+
+    console.log(this.countries);
+  },
+};
 </script>
-
-<style>
-
-</style>
