abc3ebaac163dde637ff1e8086cc5207fd3dbd6b
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-12-20 16:38:36 +0300

    ref: VCountries compositionAPI implementation

--- src/components/VCountries.vue
+++ src/components/VCountries.vue
@@ -14,33 +14,32 @@
 </template>
 
 <script>
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+
 import Dialog from "primevue/dialog";
 
 import VCountriesCard from "../components/cards/VCountriesCard";
 import VGlobalCountryInfoCard from "../components/cards/VGlobalCountryInfoCard";
-import { mapGetters } from "vuex";
 
 export default {
   name: "VCountries",
 
   components: { Dialog, VCountriesCard, VGlobalCountryInfoCard },
 
-  data() {
+  setup() {
+    const store = useStore();
+
+    const isMaximize = ref(false);
+
+    const currentIndicator = computed(
+      () => store.getters["countries/currentIndicator"]
+    );
+
     return {
-      isMaximize: false,
+      isMaximize,
+      currentIndicator,
     };
-  },
-
-  computed: {
-    ...mapGetters({
-      currentIndicator: "countries/currentIndicator",
-    }),
-  },
-
-  methods: {
-    handleChangeIndicator(indicator) {
-      this.currentIndicator = indicator;
-    },
   },
 };
 </script>
